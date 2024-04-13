0b48fd7bf10ea344a095cdc59245ad0dc23de880
ABC <70518092+Anonymous-ABC-XYZ@users.noreply.github.com>
2024-04-13 22:01:00 +0530

    Update train.py

--- train.py
+++ train.py
@@ -26,18 +26,16 @@ def trainList(station_from, station_to, rail_line):
     tablines = soup.find_all("td", class_="tabline", attrs={"style":re.compile("color:#ff4500")})
     links = soup.find_all("a", attrs={"style": re.compile("color:blue"), "title":re.compile("train")})
     for j in range(0, len(links)-1):
-        if "*" in links[j].text and current_day == 0:
-            j += 1
-        else:
-
-            car_no = links[j].text
-            for i in range(j, j+2):
-                if counter % 2 == 0:
-                    start_time = tablines[i].text 
-                else:
-                    arrival_time = tablines[i].text
+        car_no = links[j].text
+        print(car_no)
+        for i in range(counter, counter+2):
+            if counter % 2 == 0:
                 counter += 1
-            dict[str(car_no)] = {"start time": start_time.replace(u'\xa0', u' '), "arrival_time": arrival_time.replace(u'\xa0', u'') }
+                start_time = tablines[i].text 
+            else:
+                arrival_time = tablines[i].text
+                counter += 1
+        dict[str(car_no)] = {"start time": start_time.replace(u'\xa0', u' '), "arrival_time": arrival_time.replace(u'\xa0', u'') }
     print(dict)
 
 
